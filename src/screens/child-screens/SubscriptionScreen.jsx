560a9b63edb544862e13e3eb48978cd3eb74febe
MOHAMED AMEEN <ameentp64@gmail.com>
2024-08-28 10:52:36 +0530

    Subscription Management Page Completed

--- src/screens/child-screens/SubscriptionScreen.jsx
+++ src/screens/child-screens/SubscriptionScreen.jsx
@@ -1,12 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
+import SubscriptionHead from "../../components/subscription/SubscriptionHead";
+import SubscriptionTable from "../../components/subscription/SubscriptionTable";
+import SubscriptionAdd from "../../components/subscription/SubscriptionAdd";
 
 function SubscriptionScreen() {
+  const [addValue, setAddValue] = useState(false);
   return (
     <>
-      <div className="w-full h-full  flex flex-col items-center"></div>
-      {/* <div className="w-full h-[55%] mt-10">
-        <BottomContainer />
-        </div> */}
+      <div className="relative w-[90%] h-full mx-auto">
+        <div className={`grid gap-10 ${addValue ? "blur-lg" : ""}`}>
+          <SubscriptionHead setValue={setAddValue} />
+          <SubscriptionTable />
+        </div>
+        {addValue && (
+          <div className="w-full absolute top-10 ">
+            <SubscriptionAdd setValue={setAddValue} />
+          </div>
+        )}
+      </div>
     </>
   );
 }
